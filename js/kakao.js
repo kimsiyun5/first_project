e8a3c5285d78b72ec3445ce2170dc3f394b66c02
kimsiyun5 <3tldbs1@naver.com>
2021-09-25 04:32:40 +0900

    카카오 로그인 2차 테스트

--- js/kakao.js
+++ js/kakao.js
@@ -1,7 +1,32 @@
 Kakao.init('af313d8b90aca9b4f05a809c2c2b2ed3');
 
+// function kakaoLogin() {
+//     Kakao.Auth.authorize({
+//         redirectUri: 'https://kimsiyun5.github.io/first_project/index.html',
+//         success: function() {
+//             if (ACCESS_TOKEN) {
+//                 Kakao.Auth.setAccessToken(ACCESS_TOKEN);
+//             }
+//         }
+//     });
+// }
 function kakaoLogin() {
-    Kakao.Auth.authorize({
-        redirectUri: 'https://kimsiyun5.github.io/first_project/index.html'
-    });
-}
+    Kakao.Auth.login({
+      success: function (response) {
+        Kakao.API.request({
+          url: '/v2/user/me',
+          success: function (response) {
+        	  console.log(response)
+          },
+          fail: function (error) {
+            console.log(error)
+          },
+        })
+      },
+      fail: function (error) {
+        console.log(error)
+      },
+    })
+  }
+
+console.log(Kakao.Auth);
