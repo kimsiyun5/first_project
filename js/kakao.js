24eb47e33cc3eca2a613871e7e20557866aaa4d3
kimsiyun5 <3tldbs1@naver.com>
2021-09-26 14:35:25 +0900

    카카오 로그인 3차 테스트

--- js/kakao.js
+++ js/kakao.js
@@ -1,32 +1,63 @@
 Kakao.init('af313d8b90aca9b4f05a809c2c2b2ed3');
+console.log(Kakao.isInitialized());
 
 // function kakaoLogin() {
-//     Kakao.Auth.authorize({
-//         redirectUri: 'https://kimsiyun5.github.io/first_project/index.html',
-//         success: function() {
-//             if (ACCESS_TOKEN) {
-//                 Kakao.Auth.setAccessToken(ACCESS_TOKEN);
-//             }
-//         }
-//     });
+//   Kakao.Auth.authorize({
+//     redirectUri: 'http://localhost:8080/first_project/index.html'
+//   });
 // }
+
+const URLSearch = new URLSearchParams(location.search);
+if (URLSearch.get('code')) {
+
+}
+
+// function test() {
+//   Kakao.API.request({
+//     url: '/v2/user/me',
+//     success: function(response) {
+//       console.log(response);
+//       alert("당신의 이메일은 " +response.kakao_account.email +"입니다");
+//     },
+//     fail: function(error) {
+//       console.log(error)
+//     },
+//   })
+// }
+
 function kakaoLogin() {
-    Kakao.Auth.login({
-      success: function (response) {
-        Kakao.API.request({
-          url: '/v2/user/me',
-          success: function (response) {
-        	  console.log(response)
-          },
-          fail: function (error) {
-            console.log(error)
-          },
-        })
+  Kakao.Auth.login({
+    success: function(response) {
+      Kakao.API.request({
+        url: '/v2/user/me',
+        success: function(response) {
+          console.log(response);
+          alert("당신의 이메일은 " +response.kakao_account.email +"입니다");
+        },
+        fail: function(error) {
+          console.log(error)
+        },
+      })
+    },
+    fail: function(error) {
+      console.log(error)
+    },
+  })
+}
+
+function kakaoLogout() {
+  if (Kakao.Auth.getAccessToken()) {
+    Kakao.API.request({
+      url: '/v1/user/unlink',
+      success: function(response) {
+        console.log(response)
       },
-      fail: function (error) {
+      fail: function(error) {
         console.log(error)
       },
     })
+    Kakao.Auth.setAccessToken(undefined)
   }
+}
 
-console.log(Kakao.Auth);
+// console.log(Kakao.Auth);
